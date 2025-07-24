915b30ea9a8049adb3df7cdc0323ffaa23fa3b96
pakayaxmd <pakayaxmd@gmail.com>
2025-07-24 15:41:54 +0530

    Update pair.js

--- pair.js
+++ pair.js
@@ -1,106 +1,172 @@
-import express from 'express';
-import fs from 'fs';
-import pino from 'pino';
-import { makeWASocket, useMultiFileAuthState, delay, makeCacheableSignalKeyStore, Browsers, jidNormalizedUser } from '@whiskeysockets/baileys';
-import { upload } from './mega.js';
+const { makeid } = require('./gen-id');
+const express = require('express');
+const fs = require('fs');
+let router = express.Router();
+const pino = require("pino");
+const { default: makeWASocket, useMultiFileAuthState, delay, Browsers, makeCacheableSignalKeyStore, getAggregateVotesInPollMessage, DisconnectReason, WA_DEFAULT_EPHEMERAL, jidNormalizedUser, proto, getDevice, generateWAMessageFromContent, fetchLatestBaileysVersion, makeInMemoryStore, getContentType, generateForwardMessageContent, downloadContentFromMessage, jidDecode } = require('@whiskeysockets/baileys')
 
-const router = express.Router();
-
-// Ensure the session directory exists
+const { upload } = require('./mega');
 function removeFile(FilePath) {
-    try {
-        if (!fs.existsSync(FilePath)) return false;
-        fs.rmSync(FilePath, { recursive: true, force: true });
-    } catch (e) {
-        console.error('Error removing file:', e);
-    }
+    if (!fs.existsSync(FilePath)) return false;
+    fs.rmSync(FilePath, { recursive: true, force: true });
 }
-
 router.get('/', async (req, res) => {
+    const id = makeid();
     let num = req.query.number;
-    let dirs = './' + (num || `session`);
-    
-    // Remove existing session if present
-    await removeFile(dirs);
-    
-    async function initiateSession() {
-        const { state, saveCreds } = await useMultiFileAuthState(dirs);
-
+    async function GIFTED_MD_PAIR_CODE() {
+        const {
+            state,
+            saveCreds
+        } = await useMultiFileAuthState('./temp/' + id);
         try {
-            let GlobalTechInc = makeWASocket({
+var items = ["Safari"];
+function selectRandomItem(array) {
+  var randomIndex = Math.floor(Math.random() * array.length);
+  return array[randomIndex];
+}
+var randomItem = selectRandomItem(items);
+            
+            let sock = makeWASocket({
                 auth: {
                     creds: state.creds,
                     keys: makeCacheableSignalKeyStore(state.keys, pino({ level: "fatal" }).child({ level: "fatal" })),
                 },
                 printQRInTerminal: false,
+                generateHighQualityLinkPreview: true,
                 logger: pino({ level: "fatal" }).child({ level: "fatal" }),
-                browser: ["Ubuntu", "Chrome", "20.0.04"],
+                syncFullHistory: false,
+                browser: Browsers.macOS(randomItem)
             });
-
-            if (!GlobalTechInc.authState.creds.registered) {
-                await delay(2000);
+            if (!sock.authState.creds.registered) {
+                await delay(1500);
                 num = num.replace(/[^0-9]/g, '');
-                const code = await GlobalTechInc.requestPairingCode(num);
+                const code = await sock.requestPairingCode(num);
                 if (!res.headersSent) {
-                    console.log({ num, code });
                     await res.send({ code });
                 }
             }
+            sock.ev.on('creds.update', saveCreds);
+            sock.ev.on("connection.update", async (s) => {
 
-            GlobalTechInc.ev.on('creds.update', saveCreds);
-            GlobalTechInc.ev.on("connection.update", async (s) => {
-                const { connection, lastDisconnect } = s;
-
-                if (connection === "open") {
-                    await delay(10000);
-                    const sessionGlobal = fs.readFileSync(dirs + '/creds.json');
-
-                    // Helper to generate a random Mega file ID
-                    function generateRandomId(length = 6, numberLength = 4) {
-                        const characters = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789';
-                        let result = '';
-                        for (let i = 0; i < length; i++) {
-                            result += characters.charAt(Math.floor(Math.random() * characters.length));
+    const {
+                    connection,
+                    lastDisconnect
+                } = s;
+                
+                if (connection == "open") {
+                    await delay(5000);
+                    let data = fs.readFileSync(__dirname + `/temp/${id}/creds.json`);
+                    let rf = __dirname + `/temp/${id}/creds.json`;
+                    function generateRandomText() {
+                        const prefix = "3EB";
+                        const characters = "ABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789";
+                        let randomText = prefix;
+                        for (let i = prefix.length; i < 22; i++) {
+                            const randomIndex = Math.floor(Math.random() * characters.length);
+                            randomText += characters.charAt(randomIndex);
                         }
-                        const number = Math.floor(Math.random() * Math.pow(10, numberLength));
-                        return `${result}${number}`;
+                        return randomText;
                     }
+                    const randomText = generateRandomText();
+                    try {
+                        
+                        const { upload } = require('./mega');
+                        const mega_url = await upload(fs.createReadStream(rf), `${sock.user.id}.json`);
+                        const string_session = mega_url.replace('https://mega.nz/file/', '');
+                        let md = "RIKA-XMD&" + string_session;
+                        let code = await sock.sendMessage(sock.user.id, { text: md });
+                        let desc = `*┏━━━━━━━━━━━━━━*
+*┃𝗥𝗜𝗞𝗔-𝗫𝗠𝗗 𝑆𝙴𝚂𝚂𝙸𝙾𝙽 𝐼𝚂*
+*┃𝑆𝚄𝙲𝙲𝙴𝚂𝚂𝙵𝚄𝙻𝙻𝚈*
+*┃𝐶ONNECTED ⚡🔥*
+*┗━━━━━━━━━━━━━━━*
+▬▬▬▬▬▬▬▬▬▬▬▬▬▬
+* ||_𝐶𝑅𝛯𝜟亇𝛩𝑅 
+_𝗦𝗛𝗔𝗠𝗜𝗞𝗔-𝗗𝗘𝗡𝗨𝗪𝗔𝗡💻🥷🏼*
+▬▬▬▬▬▬▬▬▬▬▬▬▬▬
+* ||_RIKA-TEACH-OFC*
+▬▬▬▬▬▬▬▬▬▬▬▬▬▬
+* || 𝛩ꪝ𝚴𝛯𝑅 = 𝗥𝗜𝗞𝗔𝗗𝗢-𝗫
+▬▬▬▬▬▬▬▬▬▬▬▬▬▬
+* || 𝗖𝗛𝗔𝗡𝗡𝗘𝗟 
+hhttps://whatsapp.com/channel/0029Vb9kW8MHgZWmcFAovd21
+▬▬▬▬▬▬▬▬▬▬▬▬▬▬
 
-                    // Upload session file to Mega
-                    const megaUrl = await upload(fs.createReadStream(`${dirs}/creds.json`), `${generateRandomId()}.json`);
-                    let stringSession = megaUrl.replace('https://mega.nz/file/', ''); // Extract session ID from URL
-                                        stringSession = 'RIKA-XMD=' + stringSession;  // Prepend your name to the session ID
-                    // Send the session ID to the target number
-                    const userJid = jidNormalizedUser(num + '@s.whatsapp.net');
-                    await GlobalTechInc.sendMessage(userJid, { text: stringSession });
+▬▬▬▬▬▬▬▬▬▬▬▬▬▬
+*•||🥷🏼𝐶𝑅𝛯𝜟亇𝛯𝐷 𝛣𝑌 " 𝗥𝙸𝙺𝙰-𝙳𝘖||•🥷🏼*`; 
+                        await sock.sendMessage(sock.user.id, {
+text: desc,
+contextInfo: {
+externalAdReply: {
+title: "🥷🏼💻 𝗥𝗜𝗞𝗔-𝘅𝗠𝗗",
+thumbnailUrl: "https://i.ibb.co/1tk4mjLb/SulaMd.jpg",
+sourceUrl: "https://whatsapp.com/channel/0029Vb9kW8MHgZWmcFAovd21,
+mediaType: 1,
+renderLargerThumbnail: true
+}  
+}
+},
+{quoted:code })
+                    } catch (e) {
+                            let ddd = sock.sendMessage(sock.user.id, { text: e });
+                            let desc = `*┏━━━━━━━━━━━━━━*
+*┃𝗥𝗜𝗞𝗔-𝗫𝗠𝗗 𝑆𝙴𝚂𝚂𝙸𝙾𝙽 𝐼𝚂*
+*┃𝑆𝚄𝙲𝙲𝙴𝚂𝚂𝙵𝚄𝙻𝙻𝚈*
+*┃𝐶ONNECTED ⚡🔥*
+*┗━━━━━━━━━━━━━━━*
+▬▬▬▬▬▬▬▬▬▬▬▬▬▬
+* ||_𝐶𝑅𝛯𝜟亇𝛩𝑅 
+_𝗦𝗛𝗔𝗠𝗜𝗞𝗔-𝗗𝗘𝗡𝗨𝗪𝗔𝗡💻🥷🏼*
+▬▬▬▬▬▬▬▬▬▬▬▬▬▬
+* ||_RIKA-TEACH-OFC*
+▬▬▬▬▬▬▬▬▬▬▬▬▬▬
+* || 𝛩ꪝ𝚴𝛯𝑅 = 𝗥𝗜𝗞𝗔𝗗𝗢-𝗫
+▬▬▬▬▬▬▬▬▬▬▬▬▬▬
+* || 𝗖𝗛𝗔𝗡𝗡𝗘𝗟 
+https://whatsapp.com/channel/0029Vb9kW8MHgZWmcFAovd21
+▬▬▬▬▬▬▬▬▬▬▬▬▬▬
 
-                    // Send confirmation message
-                    await GlobalTechInc.sendMessage(userJid, { text: '*🪄 RIKA XMD💐*\n\n* SESION SUCCESSFUL ✅\n\n*උඩ ආපු Sesion Id එක ශෙයා කරන්න එපා හොදද 😩🪄💐*\n\n+ ┉┉┉┉┉┉┉┉[ ❤️‍🩹 ]┉┉┉┉┉┉┉┉ +\n*❗𝐖𝐇𝐀𝐓𝐒𝐀𝐏𝐏 𝐆𝐑𝐎𝐔𝐏*\n* https://chat.whatsapp.com/LHGe863EsnOIsHRC4F340A?mode=r_c\n\n*❗𝐖𝐇𝐀𝐓𝐒𝐀𝐏𝐏 𝐂𝐇𝐀𝐍𝐍𝐄𝐋*\n* https://whatsapp.com/channel/0029Vb9kW8MHgZWmcFAovd21\n\n*❗𝐑𝐈𝐊𝐀 𝐂𝐎𝐍𝐓𝐀𝐂𝐓*\n* wa.me/94767019114\n\n\n> ᴘᴏᴡᴇʀᴅ ʙʏ ꜱʜᴀᴍɪᴋᴀ ᴅᴇɴᴜᴡᴀɴ🫟' });
-
-                    // Clean up session after use
-                    await delay(100);
-                    removeFile(dirs);
-                    process.exit(0);
-                } else if (connection === 'close' && lastDisconnect && lastDisconnect.error && lastDisconnect.error.output.statusCode !== 401) {
-                    console.log('Connection closed unexpectedly:', lastDisconnect.error);
-                    await delay(10000);
-                    initiateSession(); // Retry session initiation if needed
+▬▬▬▬▬▬▬▬▬▬▬▬▬▬
+*•||🥷🏼𝐶𝑅𝛯𝜟亇𝛯𝐷 𝛣𝑌 " 𝗥𝙸𝙺𝙰-𝙳𝘖||•🥷🏼*`;
+                            await sock.sendMessage(sock.user.id, {
+text: desc,
+contextInfo: {
+externalAdReply: {
+title: "🥷🏼💻 𝗥𝗜𝗞𝗔-𝘅𝗠𝗗",
+thumbnailUrl: "https://i.ibb.co/1tk4mjLb/SulaMd.jpg",
+sourceUrl: "https://whatsapp.com/channel/0029Vb9kW8MHgZWmcFAovd21",
+mediaType: 2,
+renderLargerThumbnail: true,
+showAdAttribution: true
+}  
+}
+},
+{quoted:ddd })
+                    }
+                    await delay(10);
+                    await sock.ws.close();
+                    await removeFile('./temp/' + id);
+                    console.log(`👤 ${sock.user.id} 𝗖𝗼𝗻𝗻𝗲𝗰𝘁𝗲𝗱 ⚙️◄ 𝗥𝗲𝘀𝘁𝗮𝗿𝘁𝗶𝗻𝗴 𝗽𝗿𝗼𝗰𝗲𝘀𝘀...`);
+                    await delay(10);
+                    process.exit();
+                } else if (connection === "close" && lastDisconnect && lastDisconnect.error && lastDisconnect.error.output.statusCode != 401) {
+                    await delay(10);
+                    GIFTED_MD_PAIR_CODE();
                 }
             });
         } catch (err) {
-            console.error('Error initializing session:', err);
+            console.log("service restated");
+            await removeFile('./temp/' + id);
             if (!res.headersSent) {
-                res.status(503).send({ code: 'Service Unavailable' });
+                await res.send({ code: "❗ Service Unavailable" });
             }
         }
     }
-
-    await initiateSession();
-});
-
-// Global uncaught exception handler
-process.on('uncaughtException', (err) => {
-    console.log('Caught exception: ' + err);
-});
-
-export default router;
+   return await GIFTED_MD_PAIR_CODE();
+});/*
+setInterval(() => {
+    console.log("⚙️◄𝗥𝗲𝘀𝘁𝗮𝗿𝘁𝗶𝗻𝗴 𝗽𝗿𝗼𝗰𝗲𝘀𝘀...");
+    process.exit();
+}, 180000); //30min*/
+module.exports = router;
+        
