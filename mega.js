3aed70f25d95c6d93655a06dbcbd7e5f004c2c76
pakayaxmd <pakayaxmd@gmail.com>
2025-07-24 19:54:44 +0530

    Update mega.js

--- mega.js
+++ mega.js
@@ -12,8 +12,8 @@ import * as mega from 'megajs';
 
 // Mega authentication credentials
 const auth = {
-    email: 'rikatexting@gmail.com', // Replace with your Mega email
-    password: 'lasi@#321', // Replace with your Mega password
+    email: 'pakayaxmd@gmail.com', // Replace with your Mega email
+    password: 'denuwan@#321', // Replace with your Mega password
     userAgent: 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/42.0.2311.135 Safari/537.36 Edge/12.246'
 };
 
